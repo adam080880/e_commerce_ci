b4758c64a9c67403e46f00116e08ba2cfa02a8e7
muhamadadam20 <muhamadadam20@gmail.com>
2019-10-20 21:37:43 +0700

    fix cart feature

--- assets/page/landing_page.js
+++ assets/page/landing_page.js
@@ -1,8 +1,11 @@
 // import { CookiesManipulation } from "../plugins/CookiesManipulation";
 function getCartCookies(param, cookies)
-{
-    if(!cookies.getCookie('cart')) {
+{    
+    if(cookies.getCookie('cart') == false || cookies.getCookie('cart') == '') {
         param.html(0)
+    } else {
+        let res = JSON.parse(cookies.getCookie('cart'))
+        param.html(res.item.length)
     }
 }
 
